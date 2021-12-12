254e9f830e44abd4054050e6dce5c3ac740fc74d
libbies <bcw@well.com>
2021-12-12 09:36:37 -0500

    day 12 solutions

--- 2021/day12/part2.py
+++ 2021/day12/part2.py
@@ -1,28 +1,29 @@
 """advent of code 2021 day 12 part 2"""
+from collections import defaultdict
 lines = [_.split('-') for _ in open("input.txt").read().splitlines()]
 
-paths = dict()
+paths = defaultdict(list)
 for c1,c2 in lines:
-    if c1 not in paths and c2!="start":
-        paths[c1] = [c2]
-    elif c2!="start" and c2 not in paths[c1]:
+    if c2!="start" and c2 not in paths[c1]:
         paths[c1].append(c2)
-    if c2 not in paths and c1!="start":
-        paths[c2] = [c1]
-    elif c1!="start" and c1 not in paths[c2]:
+    if c1!="start" and c1 not in paths[c2]:
         paths[c2].append(c1)
 
-completed = set()
+complete = set()
 deadends = set()
 path = ["start"]
 while path:
-    if "end" in paths[path[-1]] and tuple(path+["end"]) not in completed:
-        completed.add(tuple(path+["end"]))
-    counts = [path.count(p) for p in path if p.islower()]
+    dupe = False
+    for p in path:
+        if p.islower() and path.count(p)>=2:
+            dupe = True
+            break
     for c in paths[path[-1]]:
-        if c in ("start", "end"):
+        if c=="end":
+            if tuple(path) not in complete:
+                complete.add(tuple(path))
             continue
-        if c.islower() and c in path and 2 in counts:
+        if c.islower() and c in path and dupe:
             continue
         if tuple(path+[c]) in deadends:
             continue
@@ -33,5 +34,5 @@ while path:
         if path.pop()=="start":
             break
 
-answer = len(completed)
+answer = len(complete)
 print("aoc 2021 day 12 part 2:", answer)
