589bd945ae9d1e75b5a8d77cb25be1b52f912ed3
libbies <bcw@well.com>
2021-12-15 12:26:30 -0500

    day 15 bug fix

--- 2021/day15/part2.py
+++ 2021/day15/part2.py
@@ -1,5 +1,7 @@
 #!python
 """advent of code 2021 day 15 part 2"""
+from itertools import chain
+
 lines = open("input.txt").read().splitlines()
 l = len(lines)
 riskmap = {x: {y: 0 for y in range(l*5)} for x in range(l*5)}
@@ -12,8 +14,8 @@ for x, line in enumerate(lines):
 
 l = len(riskmap)
 scoreboard = {x: {y: 0 for y in range(l)} for x in range(l)}
-# the initial base score of each position is the risk of the position itself
-# plus the lesser of the tile to the right (y+1) or below (x+1) each position
+# the initial base risk of each position is the risk of the position itself
+# plus the lesser of the neighbor to the right (y+1) or below (x+1)
 scoreboard[l-1][l-1] = riskmap[l-1][l-1]
 for n in reversed(range(l-1)):
     scoreboard[n][l-1] = riskmap[n][l-1] + scoreboard[n+1][l-1]
@@ -28,17 +30,19 @@ for x in reversed(range(l-1)):
 
 # we can then further refine each position score by comparing to their neighbors
 # and adjusting the score when a neighbor has a lower score than a position.
-# we loop until the score for the starting position has reached its minimum
+# we loop until the score for the starting position (0,0) has reached minimum
 best = scoreboard[0][0]
 while True:
-    for x in reversed(range(l-1)):
-        for y in reversed(range(l-1)):
-            scoreboard[x][y] = min(
-                riskmap[x][y] + scoreboard[x-1][y] if x>0 else best,
-                riskmap[x][y] + scoreboard[x+1][y] if x<l-1 else best,
-                riskmap[x][y] + scoreboard[x][y-1] if y>0 else best,
-                riskmap[x][y] + scoreboard[x][y+1] if x<l-1 else best,
+    for x in chain(range(l-1), reversed(range(l-1))):
+        for y in chain(range(l-1), reversed(range(l-1))):
+            new_min = riskmap[x][y] + min(
+                scoreboard[x-1][y] if x>0 else best,
+                scoreboard[x+1][y] if x<l-1 else best,
+                scoreboard[x][y-1] if y>0 else best,
+                scoreboard[x][y+1] if y<l-1 else best,
             )
+            if scoreboard[x][y] > new_min:
+                scoreboard[x][y] = new_min
     if best>scoreboard[0][0]:
         best = scoreboard[0][0]
     else:
