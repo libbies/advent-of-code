bae4df06704b10233f85ddc5ee7ee5d2ae2e43df
libbies <bcw@well.com>
2021-12-16 12:03:43 -0500

    day 16 solutions

--- 2021/day16/part2.py
+++ 2021/day16/part2.py
@@ -8,32 +8,7 @@ class Packet():
         self._raw = bits if bits else ''.join(f"{int(h, 16):0>4b}" for h in hexits)
         if self.type!=4:
             self.subpackets = self._subpackets()
-        if self.type==0:   # op:add
-            self.value = sum(p.value for p in self.subpackets)
-        elif self.type==1: # op:mul
-            self.value = reduce(int.__mul__, (p.value for p in self.subpackets))
-        elif self.type==2: # op:min
-            self.value = min(p.value for p in self.subpackets)
-        elif self.type==3: # op:max
-            self.value = max(p.value for p in self.subpackets)
-        elif self.type==4: # val
-            self._groups = []
-            for bit in [self._raw[6:][i*5:(i+1)*5] for i in range(len(self._raw[6:])//5)]:
-                self._groups.append(bit)
-                if bit[0]=="0":
-                    break
-            assert len(self._groups)>=1
-            self._len = (5*len(self._groups))+6
-            self.value = int("0b"+''.join(b[-4:] for b in self._groups), 2)
-        elif self.type==5: # op:gt
-            assert len(self.subpackets)==2
-            self.value = self.subpackets[0].value>self.subpackets[-1].value
-        elif self.type==6: # op:lt
-            assert len(self.subpackets)==2
-            self.value = self.subpackets[0].value<self.subpackets[-1].value
-        elif self.type==7: # op:cmp
-            assert len(self.subpackets)==2
-            self.value = self.subpackets[0].value==self.subpackets[-1].value
+        assert isinstance(self.value, int)
 
     def _subpackets(self):
         assert self.type!=4 # operator, self.type!=4
@@ -71,6 +46,35 @@ class Packet():
     def sublen(self):
         assert self.type!=4
         return int("0b" + self._raw[7:7+(15 if self.lentype==0 else 11)], 2)
+
+    @property
+    def value(self):
+        if self.type==0:   # op:add
+            return sum(p.value for p in self.subpackets)
+        elif self.type==1: # op:mul
+            return reduce(int.__mul__, (p.value for p in self.subpackets))
+        elif self.type==2: # op:min
+            return min(p.value for p in self.subpackets)
+        elif self.type==3: # op:max
+            return max(p.value for p in self.subpackets)
+        elif self.type==4: # val
+            self._groups = []
+            for bit in [self._raw[6:][i*5:(i+1)*5] for i in range(len(self._raw[6:])//5)]:
+                self._groups.append(bit)
+                if bit[0]=="0":
+                    break
+            assert len(self._groups)>=1
+            self._len = (5*len(self._groups))+6
+            return int("0b"+''.join(b[-4:] for b in self._groups), 2)
+        elif self.type==5: # op:gt
+            assert len(self.subpackets)==2
+            return self.subpackets[0].value>self.subpackets[-1].value
+        elif self.type==6: # op:lt
+            assert len(self.subpackets)==2
+            return self.subpackets[0].value<self.subpackets[-1].value
+        elif self.type==7: # op:cmp
+            assert len(self.subpackets)==2
+            return self.subpackets[0].value==self.subpackets[-1].value
 
     def __len__(self):
         return self._len
