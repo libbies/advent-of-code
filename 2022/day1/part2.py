229ad11e332fa7bc2ca197dc5e2be8222dcde1d7
libbies <8513169+libbies@users.noreply.github.com>
2022-12-02 00:27:41 -0500

    2022 day 1 solutions

--- 2022/day1/part2.py
+++ 2022/day1/part2.py
@@ -1,8 +1,8 @@
 #!python
 """advent of code 2022 day 1 part 2"""
 answer = sum(sorted(
-    sum(int(n) for n in _.split())
-    for _ in open("input.txt").read().split("\n\n")
+    sum(int(calorie) for calorie in elf.split())
+    for elf in open("input.txt").read().split("\n\n")
 )[-3:])
 
 print("part 2:", answer)
