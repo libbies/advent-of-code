22c11c9ee28f0fe22db6a63b1335705a82ebd91f
libbies <bcw@well.com>
2022-12-05 11:40:35 -0500

    2022 day 5 solutions

--- 2022/day5/part1.py
+++ 2022/day5/part1.py
@@ -7,9 +7,9 @@ moves = [map(int, _.split()[1::2]) for _ in lines if "move" in _]
 crates = { c: list() for c in map(int, stacks[-1].split()) }
 
 for stack in stacks[:-1]:
-    for col in range(1, max(crates.keys())*4, 4):
-        if stack[col] != " ":
-            crates[col//4+1] += stack[col]
+    for c in range(1, max(crates.keys())*4, 4):
+        if stack[c] != " ":
+            crates[c//4+1] += stack[c]
 
 for count, src, dst in moves:
     for _ in range(count):
