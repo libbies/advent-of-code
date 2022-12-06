6b4cecf218d3e58481d0564fa1f594d12319aac7
libbies <8513169+libbies@users.noreply.github.com>
2022-12-06 00:14:40 -0500

    2022 day 6 solutions

---
+++ 2022/day6/part2.py
@@ -0,0 +1,12 @@
+#!python
+"""advent of code 2022 day 6 part 2"""
+from collections import Counter
+data = open("input.txt").read().strip()
+
+for i in range(len((data))):
+    segment = data[i:i+14]
+    if max(Counter(segment).values()) == 1:
+        answer = i + 14
+        break
+
+print("part 1:", answer)
