593e63fe76e47735f0dadf85d2951c2207798b8c
libbies <8513169+libbies@users.noreply.github.com>
2022-12-06 00:21:50 -0500

    2022 day 6 solutions

--- 2022/day6/part1.py
+++ 2022/day6/part1.py
@@ -1,11 +1,9 @@
 #!python
 """advent of code 2022 day 6 part 1"""
-from collections import Counter
 data = open("input.txt").read().strip()
 
 for i in range(len((data))):
-    segment = data[i:i+4]
-    if max(Counter(segment).values()) == 1:
+    if len(set(data[i:i+4])) == 4:
         answer = i + 4
         break
 
