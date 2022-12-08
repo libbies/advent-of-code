77fa4a15e592f30c46ad469f0a3cc45e31aa0b6f
libbies <bcw@well.com>
2022-12-08 01:53:54 -0500

    2022 day 8 solutions

---
+++ 2022/day8/part2.py
@@ -0,0 +1,41 @@
+#!python
+"""advent of code 2022 day 8 part 1"""
+trees = [list(map(int, line)) for line in open("input.txt").read().splitlines()]
+length = len(trees)
+
+answer = 0
+for x in range(1, length-1):
+    for y in range(1, length-1):
+        score = [0, 0, 0, 0]
+
+        for n in range(x-1, -1, -1): # up
+            if trees[x][y] <= trees[n][y]:
+                score[0] = x-n
+                break
+        else:
+            score[0] = x
+
+        for n in range(x+1, length): # down
+            if trees[x][y] <= trees[n][y]:
+                score[1] = n-x
+                break
+        else:
+            score[1] = length-1-x
+
+        for n in range(y-1, -1, -1): # left
+            if trees[x][y] <= trees[x][n]:
+                score[2] = y-n
+                break
+        else:
+            score[2] = y
+
+        for n in range(y+1, length): # right
+            if trees[x][y] <= trees[x][n]:
+                score[3] = n-y
+                break
+        else:
+            score[3] = length-1-y
+
+        answer = max(answer, score[0] * score[1] * score[2] * score[3])
+
+print("part 2:", answer)
