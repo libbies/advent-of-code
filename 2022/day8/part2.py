4f82d9b5ee4ccd437169ff5cb097f2a55d3c7863
libbies <8513169+libbies@users.noreply.github.com>
2022-12-08 02:07:58 -0500

    2022 day 8 solutions

--- 2022/day8/part2.py
+++ 2022/day8/part2.py
@@ -8,28 +8,28 @@ for x in range(1, length-1):
     for y in range(1, length-1):
         score = [0, 0, 0, 0]
 
-        for n in range(x-1, -1, -1): # up
+        for n in range(x-1, -1, -1): # looking up
             if trees[x][y] <= trees[n][y]:
                 score[0] = x-n
                 break
         else:
             score[0] = x
 
-        for n in range(x+1, length): # down
+        for n in range(x+1, length): # looking down
             if trees[x][y] <= trees[n][y]:
                 score[1] = n-x
                 break
         else:
             score[1] = length-1-x
 
-        for n in range(y-1, -1, -1): # left
+        for n in range(y-1, -1, -1): # looking left
             if trees[x][y] <= trees[x][n]:
                 score[2] = y-n
                 break
         else:
             score[2] = y
 
-        for n in range(y+1, length): # right
+        for n in range(y+1, length): # looking right
             if trees[x][y] <= trees[x][n]:
                 score[3] = n-y
                 break
