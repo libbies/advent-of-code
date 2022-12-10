18427ea773b71bc0d91649e90887bc53d0df7368
libbies <bcw@well.com>
2022-12-10 00:55:08 -0500

    2022 day 10 solutions

--- 2022/day10/part1.py
+++ 2022/day10/part1.py
@@ -7,14 +7,14 @@ registers = { "X": 1 }
 answer = 0
 for line in lines:
     if line[0] == "noop":
-        if cycle == 20 or (cycle % 40)== 20:
+        if cycle%40==20:
             answer += cycle * registers["X"]
         cycle += 1
     elif line[0] == "addx":
-        if cycle == 20 or (cycle % 40)== 20:
+        if cycle%40==20:
             answer += cycle * registers["X"]
         cycle += 1
-        if cycle == 20 or (cycle % 40)== 20:
+        if cycle%40==20:
             answer += cycle * registers["X"]
         cycle += 1
         registers["X"] += int(line[-1])
