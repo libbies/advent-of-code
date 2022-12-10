bd85c604d07cb18914503b74f5e202941a40495b
libbies <8513169+libbies@users.noreply.github.com>
2022-12-10 00:58:25 -0500

    2022 day 10 solutions

--- 2022/day10/part1.py
+++ 2022/day10/part1.py
@@ -3,20 +3,20 @@
 lines = [line.split() for line in open("input.txt").read().splitlines()]
 
 cycle = 1
-registers = { "X": 1 }
+registers = {'X': 1}
 answer = 0
 for line in lines:
-    if line[0] == "noop":
+    if line[0]=="noop":
         if cycle%40==20:
-            answer += cycle * registers["X"]
+            answer += cycle * registers['X']
         cycle += 1
-    elif line[0] == "addx":
+    elif line[0]=="addx":
         if cycle%40==20:
-            answer += cycle * registers["X"]
+            answer += cycle * registers['X']
         cycle += 1
         if cycle%40==20:
-            answer += cycle * registers["X"]
+            answer += cycle * registers['X']
         cycle += 1
-        registers["X"] += int(line[-1])
+        registers['X'] += int(line[-1])
 
 print("part 1:", answer)
--- 2022/day10/part2.py
+++ 2022/day10/part2.py
@@ -4,21 +4,21 @@ from pprint import pprint
 lines = [line.split() for line in open("input.txt").read().splitlines()]
 
 cycle = 1
-registers = { "X": 1 }
-display = [[" "] * 40 for _ in range(6)]
+registers = { 'X': 1 }
+display = [[' '] * 40 for _ in range(6)]
 for line in lines:
     if line[0] == "noop":
-        if registers["X"] <= cycle%40 <= registers["X"]+2:
-            display[cycle//40][cycle%40] = "█"
+        if registers['X'] <= cycle%40 <= registers['X']+2:
+            display[cycle//40][cycle%40] = '█'
         cycle += 1
     elif line[0] == "addx":
-        if registers["X"] <= cycle%40 <= registers["X"]+2:
-            display[cycle//40][cycle%40] = "█"
+        if registers['X'] <= cycle%40 <= registers['X']+2:
+            display[cycle//40][cycle%40] = '█'
         cycle += 1
-        if registers["X"] <= cycle%40 <= registers["X"]+2:
-            display[cycle//40][cycle%40] = "█"
+        if registers['X'] <= cycle%40 <= registers['X']+2:
+            display[cycle//40][cycle%40] = '█'
         cycle += 1
-        registers["X"] += int(line[-1])
+        registers['X'] += int(line[-1])
 
 print("part 2:")
 print('\n'.join(''.join(_[1:]) for _ in display))
