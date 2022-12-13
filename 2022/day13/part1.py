510ab15750db389035ea4ee26db8cee984708cc4
libbies <8513169+libbies@users.noreply.github.com>
2022-12-13 09:09:08 -0500

    2022 day 13 solutions

--- 2022/day13/part1.py
+++ 2022/day13/part1.py
@@ -4,12 +4,7 @@ pairs = [[*map(eval, line.split())] for line in open("input.txt").read().split("
 
 def compare(left, right):
     if type(left)==int and type(right)==int:
-        if left < right:
-            return True
-        elif left > right:
-            return False
-        else:
-            return None
+        return (left < right) if left!=right else None
     elif type(left)==int and type(right)==list:
         return compare([left], right)
     elif type(left)==list and type(right)==int:
@@ -18,16 +13,8 @@ def compare(left, right):
         for i in range(min(len(left), len(right))):
             if compare(left[i], right[i]) is not None:
                 return compare(left[i], right[i])
-        if len(left) < len(right):
-            return True
-        elif len(left) > len(right):
-            return False
-        else:
-            return None
+        return len(left) < len(right) if len(left)!=len(right) else None
 
-answer = 0
-for i, (left, right) in enumerate(pairs):
-    if compare(left, right):
-        answer += i + 1
+answer = sum(i+1 for i, (left, right) in enumerate(pairs) if compare(left, right))
 
 print("part 1:", answer)
