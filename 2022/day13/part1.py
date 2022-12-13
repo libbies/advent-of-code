72706fb2d8559b492e1d418bcd2a3ad3b9e84c2b
libbies <bcw@well.com>
2022-12-13 01:30:08 -0500

    2022 day 13 solutions

---
+++ 2022/day13/part1.py
@@ -0,0 +1,34 @@
+#!python
+"""advent of code 2022 day 13 part 1"""
+pairs = [[*map(eval, line.split())] for line in open("input.txt").read().split("\n\n")]
+
+def compare(left, right):
+    if type(left)==int and type(right)==int:
+        if left < right:
+            return True
+        elif left > right:
+            return False
+        else:
+            return None
+    elif type(left)==int and type(right)==list:
+        return compare([left], right)
+    elif type(left)==list and type(right)==int:
+        return compare(left, [right])
+    elif type(left)==list and type(right)==list:
+        l = min(len(left), len(right))
+        for i in range(l):
+            if compare(left[i], right[i]) is not None:
+                return compare(left[i], right[i])
+        if len(left) < len(right):
+            return True
+        elif len(left) > len(right):
+            return False
+        else:
+            return None
+
+answer = 0
+for i, (left, right) in enumerate(pairs):
+    if compare(left, right):
+        answer += i + 1
+
+print("part 1:", answer)
