87c67410f4ba4d6a56dde4e38c26575e526ac8a9
libbies <8513169+libbies@users.noreply.github.com>
2022-12-18 01:27:32 -0500

    2022 day 18 solutions

--- 2022/day18/part2.py
+++ 2022/day18/part2.py
@@ -9,27 +9,25 @@ grid = defaultdict(lambda: None)
 for x, y, z in cubes:
     grid[x,y,z] = 1
 
-for x, y, z in [(0,0,0), (0,0,l), (0,0,0), (0,l,0), (0,0,0), (l,0,0)]:
+for x, y, z in ((0,0,0), (0,0,l), (0,0,0), (0,l,0), (0,0,0), (l,0,0)):
     grid[x,y,z] = -1
 
 finished = False
 while not finished:
     finished = True
     for (x,y,z), value in grid.copy().items():
-        if not (-1<=x<=l+1 and -1<=y<=l+1 and -1<=z<=l+1):
+        if value!=-1 or not (-1<=x<=l+1 and -1<=y<=l+1 and -1<=z<=l+1):
             continue
-        if value!=-1:
-            continue
-        for i, j, k in [(0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)]:
+        for i, j, k in ((0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)):
             if not grid[x+i,y+j,z+k]:
-                grid[x+i,y+j,z+k]=-1
+                grid[x+i,y+j,z+k] = -1
                 finished = False
 
 answer = 0
 for (x,y,z), value in grid.copy().items():
     if value!=1:
         continue
-    for i, j, k in [(0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)]:
+    for i, j, k in ((0,0,1), (0,0,-1), (0,1,0), (0,-1,0), (1,0,0), (-1,0,0)):
         if grid[x+i,y+j,z+k]==-1:
             answer += 1
 
