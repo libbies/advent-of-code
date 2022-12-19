17015719fc69588cd5588f4899314521fc1bd369
libbies <bcw@well.com>
2022-12-19 14:14:09 -0500

    2022 day 19 solutions

--- 2022/day19/part2.py
+++ 2022/day19/part2.py
@@ -34,28 +34,35 @@ def limit(robots, costs, index):
     """limit number of robots to max cost for that material"""
     return robots[index] < max(c[index] for c in costs)
 
+best = 0
 @functools.cache
 def iterate(minutes, costs, robots, mats):
-    if minutes == 0:
-        return mats[-1]
-    ore, clay, obsidian, geode = costs
+    global best
+    if minutes<=1 or best > mats[-1] + minutes*robots[-1] + sum(range(minutes)):
+        return mats[-1] + minutes*robots[-1]
     geodes = 0
     if cost(mats, geode):
-        geodes = max(geodes, iterate(minutes-1, costs, add(robots,(0,0,0,1)), add(sub(mats,geode),robots)))
-    elif limit(robots, costs, 2) and cost(mats, obsidian):
-        geodes = max(geodes, iterate(minutes-1, costs, add(robots,(0,0,1,0)), add(sub(mats,obsidian),robots)))
-    elif limit(robots, costs, 2) and limit(robots, costs, 1) and cost(mats, clay):
-        geodes = max(geodes, iterate(minutes-1, costs, add(robots,(0,1,0,0)), add(sub(mats,clay),robots)))
-    else:
+        geodes = max(geodes,
+                     iterate(minutes-1, costs, add(robots,(0,0,0,1)), add(sub(mats,geode),robots)))
+    elif cost(mats, obsidian) and limit(robots, costs, 2):
+        geodes = max(geodes,
+                     iterate(minutes-1, costs, add(robots,(0,0,1,0)), add(sub(mats,obsidian),robots)))
+    elif cost(mats, clay) and limit(robots, costs, 1) and limit(robots, costs, 2):
+        geodes = max(geodes,
+                     iterate(minutes-1, costs, add(robots,(0,1,0,0)), add(sub(mats,clay),robots)))
+    if cost(mats, ore) and limit(robots, costs, 0) and limit(robots, costs, 1) \
+                       and limit(robots, costs, 2):
+        geodes = max(geodes,
+                     iterate(minutes-1, costs, add(robots,(1,0,0,0)), add(sub(mats,ore),robots)))
+    if not cost(mats, geode):
         geodes = max(geodes, iterate(minutes-1, costs, robots, add(mats,robots)))
-    if limit(robots, costs, 2) and limit(robots, costs, 1) and limit(robots, costs, 0) and cost(mats, ore):
-        geodes = max(geodes, iterate(minutes-1, costs, add(robots,(1,0,0,0)), add(sub(mats,ore),robots)))
+    best = max(best, geodes)
     return geodes
 
 answer = 1
 for id, (ore, clay, obsidian, geode) in blueprints.items():
-    quality = iterate(32, (ore, clay, obsidian, geode), (1, 0, 0, 0), (0, 0, 0, 0))
-    answer *= quality
+    best = 0
+    answer *= iterate(32, (ore, clay, obsidian, geode), (1, 0, 0, 0), (0, 0, 0, 0))
 
 print("add:", add.cache_info())
 print("sub:", sub.cache_info())
