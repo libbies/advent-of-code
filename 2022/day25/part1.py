abed0c534188930cbc9def3a5f6e12a5aa35d0b0
libbies <bcw@well.com>
2022-12-25 01:47:27 -0500

    2022 day 25 solution

--- 2022/day25/part1.py
+++ 2022/day25/part1.py
@@ -1,28 +1,23 @@
 #!python
 """advent of code 2022 day 25 part 1"""
 numbers = [list(line) for line in open("input.txt").read().splitlines()]
-
-def calc(number):
-    answer = 0
-    for i, n in enumerate(reversed(number)):
-        if n.isnumeric():
-            answer += (5**i) * int(n)
-        elif n=="-":
-            answer += (5**i) * -1
-        elif n=="=":
-            answer += (5**i) * -2
-    return answer
+snafu = {
+    2: "2", "2":  2,
+    1: "1", "1":  1,
+    0: "0", "0":  0,
+    4: "-", "-": -1,
+    3: "=", "=": -2,
+}
 
 total = 0
 for number in numbers:
-    total += calc(number)
-
-snafu = {2: "2", 1: "1", 0: "0", 4: "-", 3: "="}
+    for i, n in enumerate(reversed(number)):
+        total += snafu[n] * 5**i
 
 answer = str()
 while total:
     rem = total%5
     answer = snafu[rem] + answer
-    total = (total + (-rem if rem<=2 else 5-rem))//5
+    total = (total - snafu[snafu[rem]])//5
 
 print("part 1:", answer)
