4006dd05bac48b2616b3301a57e9a59edeeb4772
libbies <8513169+libbies@users.noreply.github.com>
2023-12-01 00:55:02 -0500

    2023 day 1

---
+++ 2023/day1/part1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 1 part 1"""
+lines = open("input.txt").read().splitlines()
+
+numbers = [[n for n in line if n.isnumeric()] for line in lines]
+
+answer = 0
+for nums in numbers:
+    answer += int(nums[0]+nums[-1])
+
+print("aoc 2023 day 1 part 1:", answer)
---
+++ 2023/day1/part2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 1 part 2"""
+import re
+lines = open("input.txt").read().splitlines()
+
+for n, line in enumerate(lines):
+    tmp = list(line)
+    while True:
+        line = ''.join(tmp)
+        match = [[line.index(s), n] for n, s in enumerate([
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"]) if s in line]
+        if not match:
+            break
+        i, number = min(match)
+        tmp[i] = str(number)
+    lines[n] = ''.join(tmp)
+
+numbers = [[n for n in line if n.isnumeric()] for line in lines]
+
+answer = 0
+for nums in numbers:
+    answer += int(nums[0] + nums[-1])
+
+print("aoc 2023 day 1 part 2:", answer)
