3b7aa140027a52308ed500087cae6c129eb4bc0f
libbies <8513169+libbies@users.noreply.github.com>
2023-12-03 00:44:22 -0500

    2023 day 3 solutions

---
+++ 2023/day3/part1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 3 part 1"""
+lines = [tuple(l) for l in open("input.txt").read().splitlines()]
+
+max_x, max_y = len(lines), len(lines[0])
+numbers = dict()
+for i, line in enumerate(lines):
+    number, coords = "", []
+    for j, c in enumerate(line):
+        if not number and c.isnumeric():
+            number = c
+            coords.append((i,j))
+        elif number and c.isnumeric():
+            number += c
+            coords.append((i,j))
+        elif number and not c.isnumeric():
+            for x, y in coords:
+                numbers[x,y] = int(number)
+            number, coords = "", []
+    if number:
+        for x, y in coords:
+            numbers[x,y] = int(number)
+
+answer = 0
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '.' or c.isnumeric():
+            continue
+        if i!=0:
+            if lines[i-1][j].isnumeric():
+                answer += numbers[i-1,j]
+            else:
+                if j!=0 and lines[i-1][j-1].isnumeric():
+                    answer += numbers[i-1,j-1]
+                if j!=max_x and lines[i-1][j+1].isnumeric():
+                    answer += numbers[i-1,j+1]
+        if i!=max_x:
+            if lines[i+1][j].isnumeric():
+                answer += numbers[i+1,j]
+            else:
+                if j!=0 and lines[i+1][j-1].isnumeric():
+                    answer += numbers[i+1,j-1]
+                if j!=max_x and lines[i+1][j+1].isnumeric():
+                    answer += numbers[i+1,j+1]
+        if line[j-1].isnumeric():
+            answer += numbers[i,j-1]
+        if line[j+1].isnumeric():
+            answer += numbers[i,j+1]
+
+print("aoc 2023 day 3 part 1:", answer)
---
+++ 2023/day3/part2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 3 part 2"""
+lines = [tuple(l) for l in open("input.txt").read().splitlines()]
+
+max_x, max_y = len(lines), len(lines[0])
+numbers = dict()
+for i, line in enumerate(lines):
+    number, coords = "", []
+    for j, c in enumerate(line):
+        if not number and c.isnumeric():
+            number = c
+            coords.append((i,j))
+        elif number and c.isnumeric():
+            number += c
+            coords.append((i,j))
+        elif number and not c.isnumeric():
+            for x, y in coords:
+                numbers[x,y] = int(number)
+            number, coords = "", []
+    if number:
+        for x, y in coords:
+            numbers[x,y] = int(number)
+
+answer = 0
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '.' or c.isnumeric():
+            continue
+        parts = []
+        if i!=0:
+            if lines[i-1][j].isnumeric():
+                parts.append(numbers[i-1,j])
+            else:
+                if j!=0 and lines[i-1][j-1].isnumeric():
+                    parts.append(numbers[i-1,j-1])
+                if j!=max_x and lines[i-1][j+1].isnumeric():
+                    parts.append(numbers[i-1,j+1])
+        if i!=max_x:
+            if lines[i+1][j].isnumeric():
+                parts.append(numbers[i+1,j])
+            else:
+                if j!=0 and lines[i+1][j-1].isnumeric():
+                    parts.append(numbers[i+1,j-1])
+                if j!=max_x and lines[i+1][j+1].isnumeric():
+                    parts.append(numbers[i+1,j+1])
+        if line[j-1].isnumeric():
+            parts.append(numbers[i,j-1])
+        if line[j+1].isnumeric():
+            parts.append(numbers[i,j+1])
+        if len(parts)==2:
+            answer += parts[0] * parts[-1]
+
+print("aoc 2023 day 3 part 2:", answer)
