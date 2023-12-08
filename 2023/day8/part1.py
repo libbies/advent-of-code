d0f1ffb5ceb20e486ef9a24f982fd24ae5f29ea0
libbies <8513169+libbies@users.noreply.github.com>
2023-12-08 00:44:58 -0500

    2023 day 8 solutions

---
+++ 2023/day8/part1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 8 part 1"""
+import re
+lines = open("input.txt").readlines()
+
+dirs = lines[0].strip()
+network = dict()
+for line in lines[2:]:
+    node, l, r, _ = re.split(r"[^A-Z]+", line)
+    network[node] = (l, r)
+
+step = 0
+node = "AAA"
+while node!="ZZZ":
+    node = network[node][0] if dirs[step%len(dirs)]=='L' else network[node][-1]
+    step += 1
+
+answer = step
+print("aoc 2023 day 8 part 1:", answer)
---
+++ 2023/day8/part2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 8 part 2"""
+import re
+from math import lcm
+
+lines = open("input.txt").readlines()
+
+dirs = lines[0].strip()
+network = dict()
+for line in lines[2:]:
+    node, l, r, _ = re.split(r"[^A-Z12]+", line)
+    network[node] = (l, r)
+
+step = 0
+nodes = [node for node in network if node.endswith('A')]
+answers = list()
+while nodes:
+    nodes = [
+        network[node][0] if dirs[step%len(dirs)]=='L' else network[node][-1]
+        for node in nodes
+    ]
+    step += 1
+    for node in [n for n in nodes if n.endswith('Z')]:
+        nodes.remove(node)
+        answers.append(step)
+
+answer = lcm(*answers)
+print("aoc 2023 day 8 part 2:", answer)
