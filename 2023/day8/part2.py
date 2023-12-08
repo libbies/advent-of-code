de9c126e61de0ebaefcb072049e1419f4407bd47
libbies <8513169+libbies@users.noreply.github.com>
2023-12-08 00:52:01 -0500

    2023 day 8 solutions

--- 2023/day8/part2.py
+++ 2023/day8/part2.py
@@ -13,7 +13,7 @@ for line in lines[2:]:
 
 step = 0
 nodes = [node for node in network if node.endswith('A')]
-answers = list()
+steps = list()
 while nodes:
     nodes = [
         network[node][0] if dirs[step%len(dirs)]=='L' else network[node][-1]
@@ -22,7 +22,7 @@ while nodes:
     step += 1
     for node in [n for n in nodes if n.endswith('Z')]:
         nodes.remove(node)
-        answers.append(step)
+        steps.append(step)
 
-answer = lcm(*answers)
+answer = lcm(*steps)
 print("aoc 2023 day 8 part 2:", answer)
