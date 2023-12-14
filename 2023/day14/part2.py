d6196ceba56744e3b45d78cc9a515406bcd746c0
libbies <8513169+libbies@users.noreply.github.com>
2023-12-14 01:55:02 -0500

    2023 day 14 solutions

--- 2023/day14/part2.py
+++ 2023/day14/part2.py
@@ -9,65 +9,42 @@ def load(lines):
     return l
 
 lines = [list(l.strip()) for l in open("input.txt").readlines()]
-grids = dict()
+length = len(lines)
 dirs = ('N', 'W', 'S', 'E')
-answer = 0
-for turn in range(1, 4_000_000_001):
+grids = dict()
+answer, turn = 0, 0
+while not answer:
+    turn += 1
     direction = dirs[(turn-1)%4]
-    if direction == 'N':
-        for col in range(len(lines[0])):
+    if direction in ('N', 'S'):
+        for col in range(length):
             empty = deque()
-            for row in range(len(lines)):
+            for row in range(len(lines))[::-1 if direction=='S' else 1]:
                 if lines[row][col] == '.':
                     empty.append(row)
-                if lines[row][col] == '#':
+                elif lines[row][col] == '#':
                     empty = deque()
-                if lines[row][col] == 'O' and empty:
+                elif lines[row][col] == 'O' and empty:
                     lines[empty.popleft()][col] = 'O'
                     lines[row][col] = '.'
                     empty.append(row)
-    if direction=='W':
+    if direction in ('W', 'E'):
         for row in range(len(lines)):
             empty = deque()
-            for col in range(len(lines[0])):
+            for col in range(length)[::-1 if direction=='E' else 1]:
                 if lines[row][col] == '.':
                     empty.append(col)
-                if lines[row][col] == '#':
+                elif lines[row][col] == '#':
                     empty = deque()
-                if lines[row][col] == 'O' and empty:
+                elif lines[row][col] == 'O' and empty:
                     lines[row][empty.popleft()] = 'O'
                     lines[row][col] = '.'
                     empty.append(col)
-    if direction == 'S':
-        for col in range(len(lines[0])):
-            empty = deque()
-            for row in reversed(range(len(lines))):
-                if lines[row][col] == '.':
-                    empty.append(row)
-                if lines[row][col] == '#':
-                    empty = deque()
-                if lines[row][col] == 'O' and empty:
-                    lines[empty.popleft()][col] = 'O'
-                    lines[row][col] = '.'
-                    empty.append(row)
-    if direction=='E':
-        for row in range(len(lines)):
-            empty = deque()
-            for col in reversed(range(len(lines[0]))):
-                if lines[row][col] == '.':
-                    empty.append(col)
-                if lines[row][col] == '#':
-                    empty = deque()
-                if lines[row][col] == 'O' and empty:
-                    lines[row][empty.popleft()] = 'O'
-                    lines[row][col] = '.'
-                    empty.append(col)
-        grid = ''.join(''.join(l) for l in lines)
-        if grid in grids:
-            if (4_000_000_000 - turn) % (turn - grids[grid]) == 0:
-                answer = load(lines)
-        grids[grid] = turn
-    if answer:
-        break
+        if direction == 'E':
+            grid = ''.join(''.join(l) for l in lines)
+            if grid in grids:
+                if (4_000_000_000 - turn) % (turn - grids[grid]) == 0:
+                    answer = load(lines)
+            grids[grid] = turn
 
 print("aoc 2023 day 14 part 2:", answer)
