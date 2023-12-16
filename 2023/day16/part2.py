49eb3537ecb8e7e1fd502ab455e8b4237ca87b01
libbies <8513169+libbies@users.noreply.github.com>
2023-12-16 01:08:29 -0500

    2023 day 16 solutions

--- 2023/day16/part2.py
+++ 2023/day16/part2.py
@@ -5,48 +5,48 @@ maxlen = len(lines)
 
 starts = list()
 for n in range(maxlen):
-    starts.append(((n,-1), "R"))
-    starts.append(((-1,n), "D"))
-    starts.append(((n,maxlen), "L"))
-    starts.append(((maxlen,n), "U"))
+    starts.append((n, -1, "R"))
+    starts.append((-1, n, "D"))
+    starts.append((n, maxlen, "L"))
+    starts.append((maxlen, n, "U"))
 
 answer = 0
 queue = list()
 for start in starts:
     queue.append(start)
-    visited = [[0 for _ in range(maxlen)] for _ in range(maxlen)]
+    visited = [[0] * maxlen for _ in range(maxlen)]
     history = set()
     while queue:
-        (row, col), direction = queue.pop()
-        if ((row, col), direction) in history:
+        row, col, direction = queue.pop()
+        if (row, col, direction) in history:
             continue
-        history.add(((row, col), direction))
+        history.add((row, col, direction))
         if (direction=='R' and col+1<maxlen) or (direction=='L' and col-1>=0):
             n = col+1 if direction=='R' else col-1
             visited[row][n] = 1
             match lines[row][n]:
                 case '/':
-                    queue.append(((row, n), 'U' if direction=='R' else 'D'))
+                    queue.append((row, n, 'U' if direction=='R' else 'D'))
                 case '\\':
-                    queue.append(((row, n), 'D' if direction=='R' else 'U'))
+                    queue.append((row, n, 'D' if direction=='R' else 'U'))
                 case '|':
-                    queue.append(((row, n), 'U' if direction=='R' else 'D'))
-                    queue.append(((row, n), 'D' if direction=='R' else 'U'))
+                    queue.append((row, n, 'U' if direction=='R' else 'D'))
+                    queue.append((row, n, 'D' if direction=='R' else 'U'))
                 case _:
-                    queue.append(((row, n), 'R' if direction=='R' else 'L'))
+                    queue.append((row, n, 'R' if direction=='R' else 'L'))
         if (direction=='D' and row+1<maxlen) or (direction=='U' and row-1>=0):
             n = row+1 if direction=='D' else row-1
             visited[n][col] = 1
             match lines[n][col]:
                 case '/':
-                    queue.append(((n, col), 'L' if direction=='D' else 'R'))
+                    queue.append((n, col, 'L' if direction=='D' else 'R'))
                 case '\\':
-                    queue.append(((n, col), 'R' if direction=='D' else 'L'))
+                    queue.append((n, col, 'R' if direction=='D' else 'L'))
                 case '-':
-                    queue.append(((n, col), 'L' if direction=='D' else 'R'))
-                    queue.append(((n, col), 'R' if direction=='D' else 'L'))
+                    queue.append((n, col, 'L' if direction=='D' else 'R'))
+                    queue.append((n, col, 'R' if direction=='D' else 'L'))
                 case _:
-                    queue.append(((n, col), 'D' if direction=='D' else 'U'))
+                    queue.append((n, col, 'D' if direction=='D' else 'U'))
     answer = max(answer, sum(sum(row) for row in visited))
 
 print("aoc 2023 day 16 part 2:", answer)
