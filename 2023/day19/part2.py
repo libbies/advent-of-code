6de54cba1be2d36c9e350ffe3857ae028a62efae
libbies <8513169+libbies@users.noreply.github.com>
2023-12-19 16:12:57 -0500

    2023 day 19 solutions

--- 2023/day19/part2.py
+++ 2023/day19/part2.py
@@ -21,18 +21,18 @@ def recurse(ranges, flow, cursor=0):
         return 0
     if flow=='A':
         return prod(end-start+1 for start,end in ranges.values())
-    for rating, op, n, dst in flows[flow][cursor:]:
-        if not rating:
-            return recurse(ranges, dst)
-        start, end = ranges[rating]
-        if op=='<':
-            return (recurse({k:v if k!=rating else (start,n-1) for k,v in ranges.items()}, dst)
-                  + recurse({k:v if k!=rating else (n,end) for k,v in ranges.items()}, flow, cursor+1)
-            )
-        if op=='>':
-            return (recurse({k:v if k!=rating else (n+1,end) for k,v in ranges.items()}, dst)
-                  + recurse({k:v if k!=rating else (start,n) for k,v in ranges.items()}, flow, cursor+1)
-            )
+    rating, op, n, dst = flows[flow][cursor]
+    if not rating:
+        return recurse(ranges, dst)
+    start, end = ranges[rating]
+    if op=='<':
+        return (recurse({k:v if k!=rating else (start,n-1) for k,v in ranges.items()}, dst)
+              + recurse({k:v if k!=rating else (n,end) for k,v in ranges.items()}, flow, cursor+1)
+        )
+    if op=='>':
+        return (recurse({k:v if k!=rating else (n+1,end) for k,v in ranges.items()}, dst)
+              + recurse({k:v if k!=rating else (start,n) for k,v in ranges.items()}, flow, cursor+1)
+        )
 
 answer = recurse({k: (1, 4000) for k in "xmas"}, "in")
 print("aoc 2023 day 19 part 2:", answer)
