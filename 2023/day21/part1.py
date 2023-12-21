9d14cae16e599b2ed3d1f7445b780eb5384be816
libbies <8513169+libbies@users.noreply.github.com>
2023-12-21 02:29:33 -0500

    2023 day 21 solutions

---
+++ 2023/day21/part1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 21 part 1"""
+from collections import deque
+lines = [list(l.strip()) for l in open("input.txt").readlines()]
+
+maxlen = len(lines)
+
+steps = [[0] * maxlen for _ in range(maxlen)]
+
+for row, line in enumerate(lines):
+    if 'S' not in line:
+        continue
+    start = (row, line.index('S'))
+    break
+
+queue = [start]
+goal = 64
+for n in range(goal):
+    tmp = list()
+    for (row, col) in queue:
+        for dx, dy in ((0, 1), (1,0), (-1,0), (0,-1)):
+            if not (0<=row+dx<maxlen and 0<=col+dy<maxlen):
+                continue
+            if lines[row+dx][col+dy] == '#':
+                continue
+            if steps[row+dx][col+dy] != 0:
+                continue
+            steps[row+dx][col+dy] = n+1
+            tmp.append((row+dx,col+dy))
+    queue = tmp
+
+answer = sum(sum(1 for step in row if step and step%2==0) for row in steps)
+
+print("aoc 2023 day 21 part 1:", answer)
---
+++ 2023/day21/part2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""advent of code 2023 day 21 part 2"""
+from collections import deque
+from collections import defaultdict
+from itertools import pairwise
+lines = [list(l.strip()) for l in open("input.txt").readlines()]
+
+maxlen = len(lines)
+
+steps = defaultdict(lambda:0)
+
+for row, line in enumerate(lines):
+    if 'S' not in line:
+        continue
+    start = (row, line.index('S'))
+    break
+
+queue = [start]
+goal = 26501365
+counts = list()
+n, steps = 0, 0
+visited = set()
+while len(counts)!=3:
+    tmp = deque()
+    for (row, col) in queue:
+        for dx, dy in ((0, 1), (1,0), (-1,0), (0,-1)):
+            if lines[(row+dx)%maxlen][(col+dy)%maxlen] == '#':
+                continue
+            if (row+dx,col+dy) in visited:
+                continue
+            visited.add((row+dx,col+dy))
+            if n%2==0:
+                steps += 1
+            tmp.append((row+dx,col+dy))
+    queue = tmp
+    if n%(maxlen*2)==goal%maxlen:
+        counts.append(steps)
+    n += 1
+
+differences = [a-b for a,b in pairwise(counts[::-1])]
+cycle = [a-b for a,b in pairwise(differences)][0]
+
+answer = steps
+diff = differences[0]
+while n<=goal:
+    n += maxlen * 2
+    diff += cycle
+    answer += diff
+
+print("aoc 2023 day 21 part 2:", answer)
