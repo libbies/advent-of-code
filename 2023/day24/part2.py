eaba5186a4182428cf30dcccfff9f0457630a8e3
libbies <8513169+libbies@users.noreply.github.com>
2023-12-24 14:26:57 -0500

    2023 day 24 solutions

--- 2023/day24/part2.py
+++ 2023/day24/part2.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """advent of code 2023 day 24 part 2"""
 import re
-from z3 import Int, Ints, Solver, sat
+from z3 import Real, Reals, Solver, sat
 hail = [[int(_) for _ in re.split(r"[^0-9-]+", l.strip())] for l in open("input.txt").readlines()]
 
-Px, Py, Pz = Ints("Px Py Pz")
-Vx, Vy, Vz = Ints("Vx Vy Vz")
+Px, Py, Pz = Reals("Px Py Pz")
+Vx, Vy, Vz = Reals("Vx Vy Vz")
 
 solver = Solver()
 for i, (px, py, pz, vx, vy, vz) in enumerate(hail[:3]):
-    t_i = Int(f"t_{i}")
+    t_i = Real(f"t_{i}")
     solver.add(Px + Vx * t_i == px + vx * t_i)
     solver.add(Py + Vy * t_i == py + vy * t_i)
     solver.add(Pz + Vz * t_i == pz + vz * t_i)
