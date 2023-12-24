68ab4df78aefe141f71401389b581a7166a6b033
libbies <8513169+libbies@users.noreply.github.com>
2023-12-24 12:12:21 -0500

    2023 day 24 solutions

--- 2023/day24/part2.py
+++ 2023/day24/part2.py
@@ -4,8 +4,8 @@ import re
 from z3 import Int, Ints, Solver, sat
 hail = [[int(_) for _ in re.split(r"[^0-9-]+", l.strip())] for l in open("input.txt").readlines()]
 
-Px, Py, Pz = Ints('Px Py Pz')
-Vx, Vy, Vz = Ints('Vx Vy Vz')
+Px, Py, Pz = Ints("Px Py Pz")
+Vx, Vy, Vz = Ints("Vx Vy Vz")
 
 solver = Solver()
 for i, (px, py, pz, vx, vy, vz) in enumerate(hail[:3]):
