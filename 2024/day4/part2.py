19c7f089b25c2d24bb523a6c1afbe2bd6c2cf0ec
libbies <8513169+libbies@users.noreply.github.com>
2024-12-04 00:35:17 -0500

    aoc 2024 day 4 solutions

--- 2024/day4/part2.py
+++ 2024/day4/part2.py
@@ -14,8 +14,8 @@ for x in range(width):
     for y in range(width):
         if grid[x+1,y+1]=="A" and ((grid[x,y]=="M" and grid[x+2,y+2]=="S")
                                 or (grid[x,y]=="S" and grid[x+2,y+2]=="M")):
-            if ((grid[x+2,y]=="M" and grid[x,y+2]=="S") or
-                (grid[x+2,y]=="S" and grid[x,y+2]=="M")):
+            if ((grid[x+2,y]=="M" and grid[x,y+2]=="S")
+                    or (grid[x+2,y]=="S" and grid[x,y+2]=="M")):
                 answer += 1
 
 print("aoc 2024 day 4 part 2:", answer)
