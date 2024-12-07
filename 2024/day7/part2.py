bbed27ebee9ff3972eef3db10af79336d1267c43
libbies <8513169+libbies@users.noreply.github.com>
2024-12-07 00:18:57 -0500

    aoc 2024 day 7 solutions

---
+++ 2024/day7/part2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""advent of code 2024 day 7 part 2"""
+
+answer = 0
+for line in open("input.txt").read().splitlines():
+    test, values = line.split(':', maxsplit=1)
+    test = int(test)
+    values = [int(_) for _ in values.split()]
+    results = [values.pop(0)]
+    while values:
+        n = values.pop(0)
+        results = [ v*n for v in results] + [v+n for v in results] \
+            + [int(str(v)+str(n)) for v in results]
+    if test in results:
+        answer += test
+
+print("aoc 2024 day 7 part 2:", answer)
