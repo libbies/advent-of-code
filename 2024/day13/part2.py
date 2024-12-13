8066bda66e6f9ef7e6257d690d643a815db43414
libbies <8513169+libbies@users.noreply.github.com>
2024-12-13 08:13:44 -0500

    aoc 2024 day 13 solutions

--- 2024/day13/part2.py
+++ 2024/day13/part2.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """advent of code 2024 day 13 part 2"""
 import z3
+import re
+
+lines = open("input.txt").read().split("\n\n")
 
 pa, pb = z3.Ints("pa pb")
 answer = 0
-for _,_,ax,ay,_,_,bx,by,_,x,y in (_.split() for _ in open("input.txt").read().split("\n\n")):
+for line in lines:
+    ax,ay,bx,by,x,y = map(int, re.findall(r"[0-9]+", line))
+    x += 10_000_000_000_000
+    y += 10_000_000_000_000
     solver = z3.Optimize()
-    ax, ay, bx, by = int(ax[1:-1]), int(ay[1:]), int(bx[1:-1]), int(by[1:])
-    x, y = 10_000_000_000_000+int(x[2:-1]), 10_000_000_000_000+int(y[2:])
     solver.add(pa*ax + pb*bx == x)
     solver.add(pa*ay + pb*by == y)
     solver.add(pa>=0)
