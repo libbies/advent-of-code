5cf656ebfbc02baa6892ab2672b3a666b020ad6e
libbies <8513169+libbies@users.noreply.github.com>
2024-12-14 01:22:52 -0500

    aoc 2024 day 14 solutions

--- 2024/day14/part1.py
+++ 2024/day14/part1.py
@@ -9,10 +9,9 @@ for line in lines:
 
 height = 103
 width = 101
-for n in range(100):
-    for robot in robots:
-        robot[0] = (robot[0]+robot[2])%width
-        robot[1] = (robot[1]+robot[3])%height
+for robot in robots:
+    robot[0] = (robot[0]+robot[2]*100)%width
+    robot[1] = (robot[1]+robot[3]*100)%height
 
 q0 = sum(1 for x,y,_,_ in robots if x<width//2   and y<height//2  )
 q1 = sum(1 for x,y,_,_ in robots if   width//2<x and y<height//2  )
