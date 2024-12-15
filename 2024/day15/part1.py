0e25fa0ba81783da1a933486b2a55d85abb563ee
libbies <8513169+libbies@users.noreply.github.com>
2024-12-15 02:36:16 -0500

    aoc 2024 day 15 solutions

---
+++ 2024/day15/part1.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""advent of code 2024 day 15 part 1"""
+from collections import defaultdict
+
+lines = open("input.txt").read().split('\n\n')
+
+height = len(lines[0].split())
+width = len(lines[0].split()[0])
+
+def pprint():
+    for px in range(height):
+        for py in range(width):
+            if (px,py) == location:
+                print('@', end="")
+            elif warehouse[px,py]:
+                if (px,py) in bounds:
+                    print('█', end="")
+                else:
+                    print(warehouse[px,py], end="")
+            else:
+                print(' ', end="")
+        print()
+
+warehouse = defaultdict(str)
+bounds = set()
+for x, line in enumerate(lines[0].split()):
+    for y, char in enumerate(line):
+        if char=='.':
+            continue
+        if char=='@':
+            location = (x,y)
+            continue
+        if char=='#':
+            bounds.add((x,y))
+        warehouse[x,y] = char
+
+for move in lines[1].strip():
+    x, y = location
+    if move=='>':
+        if not warehouse[x,y+1]:
+            location = x,y+1
+        elif warehouse[x,y+1]=='O':
+            for n in range(1, width):
+                if (x,y+n) in bounds:
+                    break
+                if not warehouse[x,y+1+n]:
+                    location = x,y+1
+                    warehouse[location] = None
+                    warehouse[x,y+1+n] = 'O'
+                    break
+    if move=='<':
+        if not warehouse[x,y-1]:
+            location = x,y-1
+        elif warehouse[x,y-1]=='O':
+            for n in range(1, width):
+                if (x,y-n) in bounds:
+                    break
+                if not warehouse[x,y-1-n]:
+                    location = x,y-1
+                    warehouse[location] = None
+                    warehouse[x,y-1-n] = 'O'
+                    break
+    if move=='v':
+        if not warehouse[x+1,y]:
+            location = x+1,y
+        if warehouse[x+1,y]=='O':
+            for n in range(1, height):
+                if (x+n,y) in bounds:
+                    break
+                if not warehouse[x+1+n,y]:
+                    location = x+1,y
+                    warehouse[location] = None
+                    warehouse[x+1+n,y] = 'O'
+                    break
+    if move=='^':
+        if not warehouse[x-1,y]:
+            location = x-1,y
+        if warehouse[x-1,y]=='O':
+            for n in range(1, height):
+                if (x-n,y) in bounds:
+                    break
+                if not warehouse[x-1-n,y]:
+                    location = x-1,y
+                    warehouse[location] = None
+                    warehouse[x-1-n,y] = 'O'
+                    break
+
+pprint()
+answer = 0
+for x, y in {k for k in warehouse if warehouse[k]=='O'}:
+    score = 100 * x + y
+    answer += score
+
+print("aoc 2024 day 15 part 1:", answer)
---
+++ 2024/day15/part2.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+"""advent of code 2024 day 15 part 2"""
+from collections import defaultdict
+
+lines = open("input.txt").read().split("\n\n")
+
+height = len(lines[0].split())
+width = len(lines[0].split()[0]) * 2
+
+def pprint():
+    for px in range(height):
+        for py in range(width):
+            if (px,py) == location:
+                print('@', end="")
+            elif warehouse[px,py]:
+                if (px,py) in bounds:
+                    print('█', end="")
+                else:
+                    print(warehouse[px,py], end="")
+            else:
+                print(' ', end="")
+        print()
+
+warehouse = defaultdict(str)
+bounds = set()
+for x, line in enumerate(lines[0].split()):
+    offset = 0
+    for y, char in enumerate(line):
+        if char=='.':
+            pass
+        if char=='@':
+            location = (x,y+offset)
+        if char=='#':
+            bounds.add((x,y+offset))
+            bounds.add((x,y+offset+1))
+            warehouse[x,y+offset] = char
+            warehouse[x,y+offset+1] = char
+        if char=='O':
+            warehouse[x,y+offset] = '['
+            warehouse[x,y+offset+1] = ']'
+        offset += 1
+
+for move in lines[1].strip():
+    x, y = location
+    if move=='>':
+        if not warehouse[x,y+1]:
+            location = x,y+1
+        elif warehouse[x,y+1] in "[]":
+            for n in range(2, width):
+                if (x,y+n) in bounds:
+                    break
+                if not warehouse[x,y+n+1]:
+                    location = x,y+1
+                    warehouse[location] = None
+                    for o in range(1, n+1):
+                        warehouse[x,y+o+1] = '[' if o%2==1 else ']'
+                    break
+    if move=='<':
+        if not warehouse[x,y-1]:
+            location = x,y-1
+        elif warehouse[x,y-1] in "[]":
+            for n in range(2, width):
+                if (x,y-n) in bounds:
+                    break
+                if not warehouse[x,y-1-n]:
+                    location = x,y-1
+                    warehouse[location] = None
+                    for o in range(1, n+1):
+                        warehouse[x,y-1-o] = ']' if o%2==1 else '['
+                    break
+    if move=='v':
+        if not warehouse[x+1,y]:
+            location = x+1,y
+        elif warehouse[x+1,y] in "[]":
+            if warehouse[x+1,y] == '[':
+                boulders = [(x+1,y), (x+1,y+1)]
+            if warehouse[x+1,y] == ']':
+                boulders = [(x+1,y), (x+1,y-1)]
+            queue = boulders.copy()
+            while queue:
+                tmp = []
+                for bx, by in queue:
+                    if (bx+1,by) in bounds:
+                        break
+                    if warehouse[bx+1,by] == '[':
+                        tmp += [(bx+1,by), (bx+1,by+1)]
+                    elif warehouse[bx+1,by] == ']':
+                        tmp += [(bx+1,by), (bx+1,by-1)]
+                else:
+                    boulders += tmp
+                    queue = tmp
+                    continue
+                break
+            else:
+                tmp = warehouse.copy()
+                for bx, by in reversed(sorted(boulders)):
+                    warehouse[bx,by] = None
+                    warehouse[bx+1,by] = tmp[bx,by]
+                warehouse[location := (x+1,y)] = None
+    if move=='^':
+        if not warehouse[x-1,y]:
+            location = x-1,y
+        elif warehouse[x-1,y] in "[]":
+            if warehouse[x-1,y] == '[':
+                boulders = [(x-1,y), (x-1,y+1)]
+            if warehouse[x-1,y] == ']':
+                boulders = [(x-1,y), (x-1,y-1)]
+            queue = boulders.copy()
+            while queue:
+                tmp = []
+                for bx, by in queue:
+                    if (bx-1,by) in bounds:
+                        break
+                    if warehouse[bx-1,by] == '[':
+                        tmp += [(bx-1,by), (bx-1,by+1)]
+                    elif warehouse[bx-1,by] == ']':
+                        tmp += [(bx-1,by), (bx-1,by-1)]
+                else:
+                    boulders += tmp
+                    queue = tmp
+                    continue
+                break
+            else:
+                tmp = warehouse.copy()
+                for bx, by in sorted(boulders):
+                    warehouse[bx,by] = None
+                    warehouse[bx-1,by] = tmp[bx,by]
+                warehouse[location := (x-1,y)] = None
+
+pprint()
+answer = 0
+for x, y in {k for k in warehouse if warehouse[k]=='['}:
+    score = 100 * x + y
+    answer += score
+
+print("aoc 2024 day 15 part 2:", answer)
